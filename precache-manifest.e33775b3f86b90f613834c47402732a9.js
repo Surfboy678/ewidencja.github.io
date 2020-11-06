ab880b3d9f3a7b55fc713ccfed6267b16a927019
Surfboy678 <janusz.brodacki@gmail.com>
2020-11-06 16:28:26 +0100

    Updates

---
+++ precache-manifest.e33775b3f86b90f613834c47402732a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f31022e3cf9f9aa61c1b7bee4eb42e",
+    "url": "/Surfboy678/ewidencja.github.io.git/index.html"
+  },
+  {
+    "revision": "d28111a0be80cf81df88",
+    "url": "/Surfboy678/ewidencja.github.io.git/static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "6901bbba56c7f9cf00b8",
+    "url": "/Surfboy678/ewidencja.github.io.git/static/js/2.def6849f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Surfboy678/ewidencja.github.io.git/static/js/2.def6849f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28111a0be80cf81df88",
+    "url": "/Surfboy678/ewidencja.github.io.git/static/js/main.fbf28e0f.chunk.js"
+  },
+  {
+    "revision": "76a452179e2d86bdcad3",
+    "url": "/Surfboy678/ewidencja.github.io.git/static/js/runtime-main.750bb9fa.js"
+  }
+]);
